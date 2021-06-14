ae69828c56e658552c5ec0843b9c34753c1cdc06
David Bailey <davidbailey00@outlook.com>
2021-06-14 11:05:05 +0100

    Create colour cycling rectangle view

--- DrawingChallenge/ContentView.swift
+++ DrawingChallenge/ContentView.swift
@@ -36,23 +36,71 @@ struct Arrow: Shape {
     }
 }
 
-struct ContentView: View {
-    @State private var lineThickness: CGFloat = 0.25
+struct ColorCyclingRectangle: View {
+    var amount = 0.0
+    var steps = 100
 
     var body: some View {
-        VStack {
-            Arrow(lineThickness: lineThickness)
-                .frame(width: 150, height: 150)
-                .padding(.bottom)
+        ZStack {
+            ForEach(0 ..< steps) { value in
+                Rectangle()
+                    .inset(by: CGFloat(value))
+                    .strokeBorder(
+                        LinearGradient(
+                            gradient: Gradient(
+                                colors: [
+                                    self.color(for: value, offset: 0),
+                                    self.color(for: value, offset: 0.25)
+                                ]
+                            ),
+                            startPoint: .top,
+                            endPoint: .bottom
+                        )
+                    )
+            }
+        }
+        .drawingGroup()
+    }
 
-            Button("Toggle line thickness") {
-                withAnimation {
-                    if lineThickness == 0.75 {
-                        lineThickness = 0.25
-                    } else {
-                        lineThickness = 0.75
+    func color(for value: Int, offset: Double) -> Color {
+        var targetHue = Double(value) / Double(steps) + amount + offset
+
+        while targetHue > 1 {
+            targetHue -= 1
+        }
+
+        return Color(hue: targetHue, saturation: 1, brightness: 1)
+    }
+}
+
+struct ContentView: View {
+    @State private var lineThickness: CGFloat = 0.25
+    @State private var colorCycle = 0.0
+
+    var body: some View {
+        VStack(spacing: 64) {
+            VStack {
+                Arrow(lineThickness: lineThickness)
+                    .frame(width: 150, height: 150)
+                    .padding(.bottom)
+
+                Button("Toggle line thickness") {
+                    withAnimation {
+                        if lineThickness == 0.75 {
+                            lineThickness = 0.25
+                        } else {
+                            lineThickness = 0.75
+                        }
                     }
                 }
+            }
+
+            VStack {
+                ColorCyclingRectangle(amount: colorCycle)
+                    .frame(width: 250, height: 250)
+                    .padding(.bottom)
+
+                Slider(value: $colorCycle)
             }
         }
         .padding()
