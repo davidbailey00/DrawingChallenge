f68337b81465cf9053431c518a93103b6ecd1385
David Bailey <davidbailey00@outlook.com>
2021-06-14 10:27:06 +0100

    Initial commit

---
+++ DrawingChallenge/DrawingChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingChallengeApp.swift
+//  DrawingChallenge
+//
+//  Created by David Bailey on 14/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct DrawingChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
